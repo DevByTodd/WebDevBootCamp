1aacd1b218a92e651b1361d0eab6bb277fd9fc47
vagrant <vagrant@scotchbox>
2019-05-08 02:07:57 +0000

    all referenced and embed db files

--- Associations/readme.md
+++ Associations/readme.md
@@ -3,3 +3,14 @@
 ##Intro to Associations
 * Define Associations
 * Discuss one:one, one:many, and many:many relationships
+
+##Embedding Data
+user
+Post
+embed.js
+
+##Referencing Data
+
+##Module.Exports
+* Introduce module.exports
+* Move our models into seperate files
---
+++ Associations/referenced.js
@@ -0,0 +1,89 @@
+var mongoose = require("mongoose");
+
+//Connect to the DB
+mongoose.connect("mongodb://localhost/blog_demo_2", { 
+    useNewUrlParser: true
+// //    useCreateIndex: true 
+//     }).then(() => {
+//         console.log('connected to DB!');
+//     }).catch(err => {
+//         console.log('ERROR:', err.message);
+});
+
+
+
+// POST - title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+var Post = mongoose.model("Post", postSchema);
+
+//User - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [ 
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ]
+});
+var User = mongoose.model("User", userSchema);
+
+// User.create({
+//     email: "Bob@gmail.com",
+//     name: "Bob Belcher"
+
+// });
+
+
+// Post.create({
+//     title: "How to Cook 3",
+//     content: "blah blah Blah blah asdfasdfasdfasdfasd"
+// }, function(err, post){
+//     User.findOne({email: "Bob@gmail.com"}, function(err, foundUser){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             foundUser.posts.push(post._id);
+//             foundUser.save(function(err, data){
+//                 if(err){
+//                     console.log(err);
+//                 } else {
+//                     console.log(data);
+//                 } 
+//             });
+//         }
+//     });
+// });
+
+// User.findOne({name: "Biggiee Brown"}, function(err, user){
+//     if(err){
+//         //console.log(err);
+//     } else {
+//         user.posts.push({
+//             title: "3 thinks I really hate",
+//             content: "Vldemort. Voldemort"
+//         });
+//         user.save(function(err, user){
+//             if(err) {
+//                 console.log(err);
+//             } else {
+//                 console.log(user);
+//             }
+//         });
+//     }
+// });
+//find user
+//find all posts for that user
+
+
+User.findOne({email: "Bob@gmail.com"}).populate("posts").exec(function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(user);
+    }
+});
