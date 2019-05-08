a4a9b70da2ac9be77aec612c4745d08a8ccbb538
vagrant <vagrant@scotchbox>
2019-05-08 02:24:05 +0000

    learned models

--- Associations/referenced.js
+++ Associations/referenced.js
@@ -10,27 +10,28 @@ mongoose.connect("mongodb://localhost/blog_demo_2", {
 //         console.log('ERROR:', err.message);
 });
 
+var Post = require("./models/post");
 
+// // POST - title, content
+// var postSchema = new mongoose.Schema({
+//     title: String,
+//     content: String
+// });
+// var Post = mongoose.model("Post", postSchema);
 
-// POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-var Post = mongoose.model("Post", postSchema);
-
-//User - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [ 
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Post"
-        }
-    ]
-});
-var User = mongoose.model("User", userSchema);
+var User = require("./models/user");
+// //User - email, name
+// var userSchema = new mongoose.Schema({
+//     email: String,
+//     name: String,
+//     posts: [ 
+//         {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Post"
+//         }
+//     ]
+// });
+// var User = mongoose.model("User", userSchema);
 
 // User.create({
 //     email: "Bob@gmail.com",
@@ -39,25 +40,25 @@ var User = mongoose.model("User", userSchema);
 // });
 
 
-// Post.create({
-//     title: "How to Cook 3",
-//     content: "blah blah Blah blah asdfasdfasdfasdfasd"
-// }, function(err, post){
-//     User.findOne({email: "Bob@gmail.com"}, function(err, foundUser){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             foundUser.posts.push(post._id);
-//             foundUser.save(function(err, data){
-//                 if(err){
-//                     console.log(err);
-//                 } else {
-//                     console.log(data);
-//                 } 
-//             });
-//         }
-//     });
-// });
+Post.create({
+    title: "How to Cook part 4",
+    content: "bASDFASDFASDFA asdfasdfasdfasdfasd"
+}, function(err, post){
+    User.findOne({email: "Bob@gmail.com"}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            foundUser.posts.push(post._id);
+            foundUser.save(function(err, data){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log(data);
+                } 
+            });
+        }
+    });
+});
 
 // User.findOne({name: "Biggiee Brown"}, function(err, user){
 //     if(err){
@@ -80,10 +81,10 @@ var User = mongoose.model("User", userSchema);
 //find all posts for that user
 
 
-User.findOne({email: "Bob@gmail.com"}).populate("posts").exec(function(err, user){
-    if(err){
-        console.log(err);
-    } else {
-        console.log(user);
-    }
-});
+// User.findOne({email: "Bob@gmail.com"}).populate("posts").exec(function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
