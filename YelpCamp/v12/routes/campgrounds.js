077f3c11f450ca1a111778851f3c4a03d742b7d1
Todd Broeker <todd.broeker@gmail.com>
2019-08-12 15:09:30 +0000

    finished adding the updates to price

--- YelpCamp/v12/routes/campgrounds.js
+++ YelpCamp/v12/routes/campgrounds.js
@@ -21,13 +21,14 @@ router.get("/", function(req, res){
 router.post("/", middleware.isLoggedIn, function(req, res){
     //get data from from and add to campgrounds aray
     var name = req.body.name;
+	var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     };  
-    var newCampground =  {name: name, image: image, description: desc, author:author };
+    var newCampground =  {name: name, price: price, image: image, description: desc, author:author };
     console.log(req.user);
     //Create a new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated){
